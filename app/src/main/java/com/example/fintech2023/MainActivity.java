566bf9193c41a3d5693f2107c0a8e08236a6c12d
Stanionis, Mindaugas (ext.) <Mindaugas.Stanionis@proalpha.com>
2023-03-04 19:56:35 +0200

    Revert "Add button, to change TextView text"
    This reverts commit cf0b9d2c

--- app/src/main/java/com/example/fintech2023/MainActivity.java
+++ app/src/main/java/com/example/fintech2023/MainActivity.java
@@ -2,27 +2,13 @@ package com.example.fintech2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tvMain;
-    Button btnChangeMainText;
-    Button btnChangeMainTextColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.tvMain = findViewById(R.id.tvMain);
-        this.btnChangeMainText = findViewById(R.id.btnChangeMainText);
-        this.btnChangeMainTextColor = findViewById(R.id.btnChangeMainTextColor);
-
-        btnChangeMainText.setOnClickListener(v -> tvMain.setText("Text was changed!"));
-        btnChangeMainTextColor.setOnClickListener(v -> tvMain.setTextColor(Color.RED));
     }
 }
