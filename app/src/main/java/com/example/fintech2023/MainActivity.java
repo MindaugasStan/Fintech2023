c8e2e57cd0be027e480d67f9e1217e450ce00d90
Stanionis, Mindaugas (ext.) <Mindaugas.Stanionis@proalpha.com>
2023-03-04 19:54:03 +0200

    Add button, to change TextView color

--- app/src/main/java/com/example/fintech2023/MainActivity.java
+++ app/src/main/java/com/example/fintech2023/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fintech2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
     Button btnChangeMainText;
+    Button btnChangeMainTextColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.tvMain = findViewById(R.id.tvMain);
         this.btnChangeMainText = findViewById(R.id.btnChangeMainText);
+        this.btnChangeMainTextColor = findViewById(R.id.btnChangeMainTextColor);
 
         btnChangeMainText.setOnClickListener(v -> tvMain.setText("Text was changed!"));
+        btnChangeMainTextColor.setOnClickListener(v -> tvMain.setTextColor(Color.RED));
     }
 }
