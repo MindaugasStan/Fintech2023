cf0b9d2c3ae34db36dedb204899f428055bdfca0
Stanionis, Mindaugas (ext.) <Mindaugas.Stanionis@proalpha.com>
2023-03-04 19:49:51 +0200

    Add button, to change TextView text

--- app/src/main/java/com/example/fintech2023/MainActivity.java
+++ app/src/main/java/com/example/fintech2023/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.fintech2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
+    Button btnChangeMainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tvMain);
+        this.btnChangeMainText = findViewById(R.id.btnChangeMainText);
+
+        btnChangeMainText.setOnClickListener(v -> tvMain.setText("Text was changed!"));
     }
 }
