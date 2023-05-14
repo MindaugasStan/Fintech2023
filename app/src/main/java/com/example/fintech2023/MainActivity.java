06c766e1386d57dc7aeb32663ab8df89b73177d2
Stanionis, Mindaugas (ext.) <Mindaugas.Stanionis@proalpha.com>
2023-05-14 22:28:54 +0300

    Implementing 4.1 assignment

--- app/src/main/java/com/example/fintech2023/MainActivity.java
+++ app/src/main/java/com/example/fintech2023/MainActivity.java
@@ -2,13 +2,67 @@ package com.example.fintech2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lvNotes;
+    private ArrayAdapter listAdapter;
+    private ArrayList<String> notesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.lvNotes = findViewById(R.id.lvNotes);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        this.notesList = new ArrayList<>(sp.getStringSet("note", new HashSet<>()));
+        this.listAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, notesList);
+        this.lvNotes.setAdapter(this.listAdapter);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        //Your adapter loses reference to your list.
+        //https://stackoverflow.com/questions/15422120/notifydatasetchange-not-working-from-custom-adapter
+        notesList.clear();
+        this.notesList.addAll((sp.getStringSet("note", new HashSet<>())));
+        listAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_note_activity:
+                Intent addActivityIntent = new Intent(getApplicationContext(), com.example.fintech2023.AddNoteActivity.class);
+                startActivity(addActivityIntent);
+                return true;
+            case R.id.delete_note_activity:
+                Intent deleteActivityIntent = new Intent(getApplicationContext(), com.example.fintech2023.DeleteNoteActivity.class);
+                startActivity(deleteActivityIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
